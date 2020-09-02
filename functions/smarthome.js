ba619110720b2ee172fb03ecb1507f53c1dc87b8
Lorenzo <lollobaldo2000@gmail.com>
2020-09-02 02:22:02 +0200

    add func

---
+++ functions/smarthome.js
@@ -0,0 +1,126 @@
+const { smarthome } = require('actions-on-google');
+const mqtt = require('async-mqtt');
+
+// Hardcoded user ID
+const USER_ID = '123';
+
+const app = smarthome();
+
+app.onSync((body) => {
+  return {
+    requestId: body.requestId,
+    payload: {
+      agentUserId: USER_ID,
+      devices: [{
+        id: 'washer',
+        type: 'action.devices.types.LIGHT',
+        traits: [
+          'action.devices.traits.Brightness',
+          'action.devices.traits.ColorSetting',
+          'action.devices.traits.OnOff',
+          'action.devices.traits.LightEffects',
+        ],
+        name: {
+          defaultNames: ['My LEDs'],
+          name: 'Leds',
+          nicknames: ['Light', 'Lights', 'Led', 'Leds'],
+        },
+        deviceInfo: {
+          manufacturer: 'Lorenzo Baldini',
+          model: 'Leds-ESP-0.9',
+          hwVersion: '0.9',
+          swVersion: '0.9.0',
+        },
+        willReportState: false,
+        attributes: {
+          pausable: false,
+          commandOnlyColorSetting: true,
+        },
+      }],
+    },
+  };
+});
+
+app.onQuery(async (body) => {
+  return {};
+});
+
+
+
+const updateDevice = async (execution, deviceId) => {
+  const {params, command} = execution;
+  let state;
+  let ref;
+  switch (command) {
+    case 'action.devices.commands.OnOff':
+      state = {on: params.on};
+      ref = firebaseRef.child(deviceId).child('OnOff');
+      break;
+    case 'action.devices.commands.StartStop':
+      state = {isRunning: params.start};
+      ref = firebaseRef.child(deviceId).child('StartStop');
+      break;
+    case 'action.devices.commands.PauseUnpause':
+      state = {isPaused: params.pause};
+      ref = firebaseRef.child(deviceId).child('StartStop');
+      break;
+    default:
+      return;
+  }
+
+  return ref.update(state)
+      .then(() => state);
+};
+
+app.onExecute(async (body) => {
+  const {requestId} = body;
+  // Execution results are grouped by status
+  const result = {
+    ids: [],
+    status: 'SUCCESS',
+    states: {
+      online: true,
+    },
+  };
+
+  const client = await mqtt.connectAsync(
+    'mqtts://mqtt.flespi.io', {
+      username: '6j7r0OrwO8ReQmZk0ZszVe6hvAB8IS4E1ZUPBbbe7QiN28VQVddEg9LBxay3QqyF',
+      port: 443,
+  });
+  
+  await client.publish('logs', 'connected');
+
+  const executePromises = [];
+  const intent = body.inputs[0];
+  for (const command of intent.payload.commands) {
+    for (const device of command.devices) {
+      for (const execution of command.execution) {
+        executePromises.push(
+          updateDevice(execution, device.id)
+            .then((data) => {
+              result.ids.push(device.id);
+              Object.assign(result.states, data);
+            })
+            .catch(() => functions.logger.error('EXECUTE', device.id)));
+      }
+    }
+  }
+
+  await Promise.all(executePromises);
+  return {
+    requestId: requestId,
+    payload: {
+      commands: [result],
+    },
+  };
+});
+
+
+app.onDisconnect((body, headers) => {
+  functions.logger.log('User account unlinked from Google Assistant');
+  // Return empty response
+  return {};
+});
+
+exports.handler = functions.https.onRequest(app);
