5cf8a6202e61a5988355b1e812459195538cecd7
Lorenzo <lollobaldo2000@gmail.com>
2020-09-02 02:18:07 +0200

    add things

---
+++ functions/fakeauth.js
@@ -0,0 +1,8 @@
+exports.handler = functions.https.onRequest((request, response) => {
+  const responseurl = util.format('%s?code=%s&state=%s',
+      decodeURIComponent(request.query.redirect_uri), 'xxxxxx',
+      request.query.state);
+  functions.logger.log(`Set redirect as ${responseurl}`);
+  return response.redirect(
+      `/login?responseurl=${encodeURIComponent(responseurl)}`);
+});
