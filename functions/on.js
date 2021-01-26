f47f263cad989642fda46c3ff04472f8bed4682c
Lorenzo <lollobaldo2000@gmail.com>
2021-01-26 21:45:35 +0100

    Create on.js

---
+++ functions/on.js
@@ -0,0 +1,35 @@
+const mqtt = require('async-mqtt');
+
+const devicesChannels = {
+  'floor-lamp': 'lights/bulbs',
+  'leds-light': 'lights/leds',
+};
+
+const map = (value, lowFrom, highFrom, lowTo, highTo) => (
+  // eslint-disable-next-line no-mixed-operators
+  (value - lowFrom) * (highTo - lowFrom) / (highFrom - lowFrom) + lowTo
+);
+
+const asyncMsg = (client) => new Promise((resolve) => client.on('message', (_, msg) => resolve(msg)));
+
+const message2state = (message) => {
+  if (message && (message.charAt(0) === 'N' || message.charAt(0) === 'F')) {
+    const [brightness, temperature] = message.substring(1).split(',').map(Number);
+    const state = message.charAt(0) === 'N';
+    return { state, brightness, temperature };
+  }
+  console.error('Invalid message');
+  return {};
+};
+
+exports.handler = async function (event, context) {
+  const client = await mqtt.connectAsync(
+    'mqtts://mqtt.flespi.io', {
+      username: 'Djd77fBUcRepR3q1RveiU2sggtd1iDuLKvJIA8qANuOum4l3nn97dqbiJe9SFrre',
+      port: 8883,
+      clientId: `action-on-google--${Math.random().toString(16).substr(2, 8)}`,
+    },
+  );
+  await client.publish('lights/bulbs', 'N255,0');
+  client.end();
+};
